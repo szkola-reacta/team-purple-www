6d7546dc4c4c1f9dacd8166d2ae6f2d92de1e2e7
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-24 22:46:03 +0200

    ready site about our goal

--- purple-team/src/App.js
+++ purple-team/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+    (
+        <div className={styles.page}>
+            <h1 className={styles.header}> Wspólny cel</h1>
+            <section className={styles.wrapper}>
+                <img src={logo} className={styles.logo} alt="react logo"/>
+                <section className={styles.content}>
+                    <p>
+                        Naszym głównym celem jest poznanie Reacta, poprzez tworzenie projektu budowanego przez cały
+                        zespół.
+                        Przez kolejne tygodnie stworzymy aplikację webową o tematyce wyznaczonej przez naszego
+                        prowadzącego - Patryka. Postępy będziemy umieszczać na pltaformie Github. Innymi zadaniami,
+                        które przed sobą stawiamy to nauka pracy w grupie i współpraca przy tworzeniu kodu.
+                        Końcowm etapem naszej pracy będzie umieszczenie w internecie w pełni działającej i
+                        przetestowanej aplikacji, którą każdy z nas będzie mógł umieścić w swoim portfolio.
+                    </p>
+                </section>
+            </section>
+        </div>
+    );
+
 
 export default App;
