14629147d4b716c91f9fd80714c6684aa96ecd84
michnja <aleksandra.michniewicz97@gmail.com>
2020-05-25 23:45:14 +0200

    added footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import StartView from './StartView.js'
+import StartView from './StartView.js';
 import '../css/App.css';
+import Footer from './Footer.js';
 
 class App extends Component {
-  state = {}
+  state = {};
   render() {
     return (
-      <div className="wrapper">
-        <header className="navigation"></header>
+      <div className='wrapper'>
+        <header className='navigation'></header>
         <main>
           <StartView />
-
-
+          <Footer />
         </main>
-
       </div>
     );
   }
