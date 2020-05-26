2e70f5f8cde147c2496c797a6ebec8f5130a903c
michnja <45687768+michnja@users.noreply.github.com>
2020-05-26 11:39:21 +0200

    added footer (#13)

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer-box'>
+      <h1>#team-purple</h1>
+    </div>
+  );
+};
+
+export default Footer;
