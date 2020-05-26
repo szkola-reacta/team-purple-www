1ceea360c56910607df07c3a9c2a8c943445bf1b
Łukasz Czuba <lukaszpetra@gmail.com>
2020-05-26 11:54:05 +0200

    new section future_project (#14)
    Co-authored-by: Jędrzej Knapik <64689268+jedrek3@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
+
 import React, { Component } from 'react';
-import StartView from './StartView.js';
-import '../css/App.css';
+import StartView from './StartView.js'
+import FutureProject from './FutureProjects';
 import Footer from './Footer.js';
+import '../css/App.css';
 
 class App extends Component {
-  state = {};
+  state = {}
   render() {
     return (
-      <div className='wrapper'>
-        <header className='navigation'></header>
+      <div className="wrapper">
+        <header className="navigation"></header>
         <main>
           <StartView />
+          <FutureProject/>
           <Footer />
         </main>
+
       </div>
     );
   }
 }
-
 export default App;
+
---
+++ src/components/FutureProjects.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import '../css/FutureProject.css';
+
+const FutureProjects = () => {
+    return(
+        <div className = "container" >
+            <h1>#our projects</h1>
+            <div className="project">
+                <p>Przyszły opis projektu...</p>
+            </div>
+            <div className="project">
+            <p>Przyszły opis projektu...</p>
+            </div>
+            <div className="project">
+            <p>Przyszły opis projektu...</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default FutureProjects;
